93eefbf53b438bb5eb2a39376f3479bdbf3c7dd7
Ramata Diallo <rama.diallo@gmail.com>
2017-02-14 12:31:35 -0500

    working on the email form

--- mail/contact-me.php
+++ mail/contact-me.php
@@ -1,30 +1,58 @@
 <?php
-// Check for empty fields
-if(empty($_POST['name'])  		||
-   empty($_POST['email']) 		||
-   empty($_POST['phone']) 		||
-   empty($_POST['message'])	||
-   !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
-   {
-	echo "No arguments Provided!";
-	return false;
-   }
+/*
+From http://www.html-form-guide.com
+This is the simplest emailer one can have in PHP.
+If this does not work, then the PHP email configuration is bad!
+*/
+$msg="";
+if(isset($_POST['submit']))
+{
+    /* ****Important!****
+    replace name@your-web-site.com below
+    with an email address that belongs to
+    the website where the script is uploaded.
+    For example, if you are uploading this script to
+    www.my-web-site.com, then an email like
+    form@my-web-site.com is good.
+    */
 
-$name = strip_tags(htmlspecialchars($_POST['name']));
-$email_address = strip_tags(htmlspecialchars($_POST['email']));
-$phone = strip_tags(htmlspecialchars($_POST['phone']));
-$message = strip_tags(htmlspecialchars($_POST['message']));
+	$from_add = "form@https://ramata.github.io/";
 
-// Create the email and send the message
-$to = 'rama.diallo@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
-$email_subject = "Website Contact Form:  $name";
-$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
-$headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
-$headers .= "Reply-To: $email_address";
-$mail=mail($to, "Subject: $email_subject",$message );
-if($mail){
-  echo "Thank you for using our mail form";
-}else{
-  echo "Mail sending failed.";
+	$to_add = "rama.diallo@gmail.com"; //<-- put your yahoo/gmail email address here
 
+	$subject = "Test Subject";
+	$message = "Test Message";
+
+	$headers = "From: $from_add \r\n";
+	$headers .= "Reply-To: $from_add \r\n";
+	$headers .= "Return-Path: $from_add\r\n";
+	$headers .= "X-Mailer: PHP \r\n";
+
+
+	if(mail($to_add,$subject,$message,$headers))
+	{
+		$msg = "Mail sent OK";
+	}
+	else
+	{
+ 	   $msg = "Error sending email!";
+	}
+}
 ?>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+	<title>Test form to email</title>
+</head>
+
+<body>
+<?php echo $msg ?>
+<p>
+<form action='<?php echo htmlentities($_SERVER['PHP_SELF']); ?>' method='post'>
+<input type='submit' name='submit' value='Submit'>
+</form>
+</p>
+
+
+</body>
+</html>
